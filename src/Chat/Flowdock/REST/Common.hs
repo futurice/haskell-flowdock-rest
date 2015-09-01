b1c96905be337e72a5419d2851ad119ce06c5c66
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-09-01 09:16:28 +0300

    Move parseApiUrl to URLs module

--- src/Chat/Flowdock/REST/Common.hs
+++ src/Chat/Flowdock/REST/Common.hs
@@ -3,15 +3,8 @@ module Chat.Flowdock.REST.Common (
   UserId,
   OrganisationId,
   ApiUrl,
-  parseApiUrl,
   ParamName,
   mkParamName,
   ) where
 
-import Control.Monad.Catch
-import Network.HTTP.Client
-
 import Chat.Flowdock.REST.Internal
-
-parseApiUrl :: MonadThrow m => ApiUrl a -> m Request
-parseApiUrl (ApiUrl url) = parseUrl url
